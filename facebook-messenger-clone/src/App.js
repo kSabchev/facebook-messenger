2ecb7f0690faddbc3b54ea1eb723f854cfadfa20
kSabchev <kaloyansabchev@gmail.com>
2020-10-18 21:56:38 +0300

    basic setup with material ui components

--- facebook-messenger-clone/src/App.js
+++ facebook-messenger-clone/src/App.js
@@ -1,27 +1,69 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import { Button, FormControl, Input, InputLabel } from "@material-ui/core";
+import Message from './Message';
+
 
 function App() {
-  const [input, setInput] = useState("");
-  const [messages, setMessages] = useState(['hi']);
 
-  console.log(input);
-  console.log(messages);
+  // useState - set shot term memory
+  // set up a basic variable in react
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState(["hi"]);
+  const [username,setUsername] = useState("");
+
+  // useEfect - snipet of code that gets executed on a condition
+  useEffect(()=>{
+
+    //run code here..
+    // if it`s blank inside [], this code runs ONCE when the app component loads
+
+    setUsername(prompt('Please enter your name'));
+  },[]) // condition
+
+
+  // console.log(input);
+  // console.log(messages);
+
 
   const sendMessage = (event) => {
+    event.preventDefault();
+
     // all the logic to send the message goes here
     setMessages([...messages, input]);
-    setInput('')
+    setInput("");
   };
 
   return (
     <div className="App">
       <h1>hello</h1>
+      <h2> Wellcome {username}</h2>
 
-      <input value={input} onChange={(event) => setInput(event.target.value)} />
-      <button onClick={sendMessage}>Send Message</button>
-      
+      <form>
+        <FormControl>
+          <InputLabel>Enter a message..</InputLabel>
+          <Input
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
+        <Button
+          disabled={!input}
+          variant="contained"
+          color="primary"
+          type="submit"
+          onClick={sendMessage}
+        >
+          Send Message
+        </Button>
+        </FormControl>
+
+       
+      </form>
       {/* messages */}
+
+      {messages.map((message) => (
+        <Message text={message} />
+      ))}
     </div>
   );
 }
