3a9cf2db4cca796fcdb617c5f891961623271218
kSabchev <kaloyansabchev@gmail.com>
2020-10-25 09:17:31 +0200

    testing with firebase

--- facebook-messenger-clone/src/App.js
+++ facebook-messenger-clone/src/App.js
@@ -1,39 +1,60 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import { Button, FormControl, Input, InputLabel } from "@material-ui/core";
-import Message from './Message';
-
+import Message from "./Message";
+import db from "./firebase";
+import firebase from "firebase";
 
 function App() {
-
   // useState - set shot term memory
   // set up a basic variable in react
   const [input, setInput] = useState("");
-  const [messages, setMessages] = useState([
-    {username: 'test' , text: 'hey'},
-    {username:'test2', text:"yoo"}
-  ]);
-  const [username,setUsername] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [username, setUsername] = useState("");
 
   // useEfect - snipet of code that gets executed on a condition
-  useEffect(()=>{
-
+  useEffect(() => {
     //run code here..
     // if it`s blank inside [], this code runs ONCE when the app component loads
 
-    setUsername(prompt('Please enter your name'));
-  },[]) // condition
+    setUsername(prompt("Please enter your name"));
+  }, []); // condition
 
+  useEffect(() => {
+    // run once when app component loads
+
+    // db.collection('messages').onSnapshot(snapshot =>{
+    //   console.log(`------------`)
+    //   console.log(snapshot.docs)
+    //   console.log(`------------`)
+    //   setMessages(snapshot.docs.map(doc => doc.data))
+    // })
+    db.collection("messages")
+      .get()
+      .then((querySnapshot) => {
+        console.log( querySnapshot.docs.map((doc) => doc.data))
+        setMessages(
+          querySnapshot.docs.map((doc) => doc.data)
+          // .forEach((doc) => {
+          // console.log(doc.data());
+        );
+      });
+  }, []);
 
   // console.log(input);
   // console.log(messages);
 
-
   const sendMessage = (event) => {
     event.preventDefault();
 
+    db.collection("messages").add({
+      message: input,
+      username: username,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+
     // all the logic to send the message goes here
-    setMessages([...messages, {username: username, text: input}]);
+    // setMessages([...messages, {username: username, message: input}]);
     setInput("");
   };
 
@@ -46,21 +67,19 @@ function App() {
         <FormControl>
           <InputLabel>Enter a message..</InputLabel>
           <Input
-          value={input}
-          onChange={(event) => setInput(event.target.value)}
-        />
-        <Button
-          disabled={!input}
-          variant="contained"
-          color="primary"
-          type="submit"
-          onClick={sendMessage}
-        >
-          Send Message
-        </Button>
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+          />
+          <Button
+            disabled={!input}
+            variant="contained"
+            color="primary"
+            type="submit"
+            onClick={sendMessage}
+          >
+            Send Message
+          </Button>
         </FormControl>
-
-       
       </form>
       {/* messages */}
 
---
+++ facebook-messenger-clone/src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+        apiKey: "AIzaSyBY2GXhtiVBA-7RlCwOAOgMbvwEPaLrHrA",
+        authDomain: "facebook-messenger-clone-8a228.firebaseapp.com",
+        databaseURL: "https://facebook-messenger-clone-8a228.firebaseio.com",
+        projectId: "facebook-messenger-clone-8a228",
+        storageBucket: "facebook-messenger-clone-8a228.appspot.com",
+        messagingSenderId: "509398167874",
+        appId: "1:509398167874:web:d042df575d1a13cee798db",
+        measurementId: "G-KFF1BEWY5L"
+});
+
+const db = firebaseApp.firestore();
+
+export default db;
